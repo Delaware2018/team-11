068f29e95069002e3853b03d515d6eac591adf24
Rahul Vig <rahvee51395@gmail.com>
2018-09-22 05:27:35 -0400

    Updated with sql

---
+++ Home/app.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://username:password@localhost/db_name'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(120), unique=True)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+admin = User('admin', 'admin@example.com')
+
+db.create_all() # In case user table doesn't exists already. Else remove it.
+
+db.session.add(admin)
+
+db.session.commit() # This is needed to write the changes to database
+
+User.query.all()
+
+User.query.filter_by(username='admin').first()
